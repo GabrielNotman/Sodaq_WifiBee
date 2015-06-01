a9839be1c82d069928ede5fed0ac603cad2aedf4
Gabriel Notman <g.notman@gmail.com>
2015-06-01 17:45:49 +0100

    Added formatting changes based on the SODAQ2 formatting rules.

--- Sodaq_WifiBee.cpp
+++ Sodaq_WifiBee.cpp
@@ -1,28 +1,29 @@
 /* The MIT License (MIT)
 
-Copyright (c) <2015> <Gabriel Notman & M2M4ALL BV>
+ Copyright (c) <2015> <Gabriel Notman & M2M4ALL BV>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE. */
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE. */
 
 #include "Sodaq_WifiBee.h"
 
-Sodaq_WifiBee::Sodaq_WifiBee() {
+Sodaq_WifiBee::Sodaq_WifiBee()
+{
   _APN = "";
   _username = "";
   _password = "";
@@ -31,7 +32,8 @@ Sodaq_WifiBee::Sodaq_WifiBee() {
   _diagStream = NULL;
 }
 
-void Sodaq_WifiBee::init(HardwareSerial& stream, const uint32_t baudrate) {
+void Sodaq_WifiBee::init(HardwareSerial& stream, const uint32_t baudrate)
+{
   _dataStream = &stream;
   _dataStream->begin(DEFAULT_BAUD);
 
@@ -46,7 +48,7 @@ void Sodaq_WifiBee::init(HardwareSerial& stream, const uint32_t baudrate) {
   send(data.c_str());
   readForTime(RESPONSE_TIMEOUT);
   delay(2500);
-  
+
   //Start the port again at new rate
   _dataStream->begin(baudrate);
 
@@ -57,79 +59,97 @@ void Sodaq_WifiBee::init(HardwareSerial& stream, const uint32_t baudrate) {
   sleep();
 }
 
-void Sodaq_WifiBee::connectionSettings(char* APN, char* username, char* password) {
+void Sodaq_WifiBee::connectionSettings(char* APN, char* username,
+    char* password)
+{
   _APN = APN;
   _username = username;
   _password = password;
 }
 
-void Sodaq_WifiBee::setDiag(Stream& stream) {
+void Sodaq_WifiBee::setDiag(Stream& stream)
+{
   _diagStream = &stream;
 }
 
-void Sodaq_WifiBee::sleep() {
+void Sodaq_WifiBee::sleep()
+{
 
 }
 
-void Sodaq_WifiBee::wake() {
+void Sodaq_WifiBee::wake()
+{
 
 }
 
 // HTTP methods
-uint16_t Sodaq_WifiBee::HTTPAction(const char* server, const uint16_t port, const char* method,
-  const char* location, const char* headers, const char* body) {
+uint16_t Sodaq_WifiBee::HTTPAction(const char* server, const uint16_t port,
+    const char* method, const char* location, const char* headers,
+    const char* body)
+{
   return 0;
 }
 
-uint16_t Sodaq_WifiBee::HTTPGet(const char* server, const uint16_t port, const char* location,
-  const char* headers) {
+uint16_t Sodaq_WifiBee::HTTPGet(const char* server, const uint16_t port,
+    const char* location, const char* headers)
+{
   return 0;
 }
 
-uint16_t Sodaq_WifiBee::HTTPPost(const char* server, const uint16_t port, const char* location,
-  const char* headers, const char* body) {
+uint16_t Sodaq_WifiBee::HTTPPost(const char* server, const uint16_t port,
+    const char* location, const char* headers, const char* body)
+{
   return 0;
 }
 
 // TCP methods
-bool Sodaq_WifiBee::openTCP(const char* server, uint16_t port) {
+bool Sodaq_WifiBee::openTCP(const char* server, uint16_t port)
+{
   return openConnection(server, port, TCP_CONNECTION);
 }
 
-bool Sodaq_WifiBee::sendTCPData(const uint8_t* data, const size_t length) {
+bool Sodaq_WifiBee::sendTCPData(const uint8_t* data, const size_t length)
+{
   return sendData(data, length);
 }
 
-bool Sodaq_WifiBee::closeTCP() {
+bool Sodaq_WifiBee::closeTCP()
+{
   return closeConnection();
 }
 
 // UDP methods
-bool Sodaq_WifiBee::openUDP(const char* server, uint16_t port) {
+bool Sodaq_WifiBee::openUDP(const char* server, uint16_t port)
+{
   return openConnection(server, port, UDP_CONNECTION);
 }
 
-bool Sodaq_WifiBee::sendUDPData(const uint8_t* data, const size_t length) {
+bool Sodaq_WifiBee::sendUDPData(const uint8_t* data, const size_t length)
+{
   return sendData(data, length);
 }
 
-bool Sodaq_WifiBee::closeUDP() {
+bool Sodaq_WifiBee::closeUDP()
+{
   return closeConnection();
 }
 
-void Sodaq_WifiBee::readResponse(uint8_t buffer, const size_t size) {
+void Sodaq_WifiBee::readResponse(uint8_t buffer, const size_t size)
+{
   //Send command to dump 
   //Read size-1 or response size
 }
 
-void Sodaq_WifiBee::readHTTPResponse(uint8_t buffer, const size_t size) {
+void Sodaq_WifiBee::readHTTPResponse(uint8_t buffer, const size_t size)
+{
   //Send command to dump
   //Read until empty line
   //Save the rest until buffer is full or response is complete
 }
 
 // Private methods
-void Sodaq_WifiBee::flushInputStream() {
+void Sodaq_WifiBee::flushInputStream()
+{
   if (_dataStream) {
     while (_dataStream->available()) {
       diagPrint(_dataStream->read());
@@ -137,26 +157,26 @@ void Sodaq_WifiBee::flushInputStream() {
   }
 }
 
-void Sodaq_WifiBee::readForTime(const uint16_t timeMS) {
+void Sodaq_WifiBee::readForTime(const uint16_t timeMS)
+{
   if (!_dataStream) {
     return;
   }
 
   uint16_t time = 0;
-  while (time < timeMS)
-  {
+  while (time < timeMS) {
     if (_dataStream->available()) {
       char c = _dataStream->read();
       diagPrint(c);
-    }
-    else {
+    } else {
       time += 10;
       delay(10);
     }
   }
 }
 
-bool Sodaq_WifiBee::readTillPrompt(const char* prompt, const uint16_t timeMS) {
+bool Sodaq_WifiBee::readTillPrompt(const char* prompt, const uint16_t timeMS)
+{
   if (!_dataStream) {
     return false;
   }
@@ -171,18 +191,22 @@ bool Sodaq_WifiBee::readTillPrompt(const char* prompt, const uint16_t timeMS) {
   return true; // result;
 }
 
-void Sodaq_WifiBee::send(const char* data) {
+void Sodaq_WifiBee::send(const char* data)
+{
   if (_dataStream) {
     _dataStream->println(data);
   }
 }
 
-bool Sodaq_WifiBee::sendWaitForPrompt(const char* data, const char* prompt) {
+bool Sodaq_WifiBee::sendWaitForPrompt(const char* data, const char* prompt)
+{
   send(data);
   return readTillPrompt(prompt, RESPONSE_TIMEOUT);
 }
 
-bool Sodaq_WifiBee::openConnection(const char* server, const uint16_t port, const char* type) {
+bool Sodaq_WifiBee::openConnection(const char* server, const uint16_t port,
+    const char* type)
+{
   wake();
   connect();
 
@@ -206,14 +230,16 @@ bool Sodaq_WifiBee::openConnection(const char* server, const uint16_t port, cons
   sendWaitForPrompt(data.c_str(), LUA_PROMPT);
 
   data = String("wifiConn:on(\"receive\", ") + RECEIVED_CALLBACK + ")";
-  sendWaitForPrompt(data.c_str(), LUA_PROMPT);;
+  sendWaitForPrompt(data.c_str(), LUA_PROMPT);
+  ;
 
   //Open the connection to the server
   data = String("wifiConn:connect(") + port + ",\"" + server + "\")";
   return sendWaitForPrompt(data.c_str(), CONNECT_PROMPT);
 }
 
-bool Sodaq_WifiBee::sendData(const uint8_t* data, const size_t length) {
+bool Sodaq_WifiBee::sendData(const uint8_t* data, const size_t length)
+{
   if (!_dataStream) {
     return false;
   }
@@ -225,18 +251,20 @@ bool Sodaq_WifiBee::sendData(const uint8_t* data, const size_t length) {
   return readTillPrompt(LUA_PROMPT, RESPONSE_TIMEOUT);
 }
 
-bool Sodaq_WifiBee::closeConnection() {
+bool Sodaq_WifiBee::closeConnection()
+{
   String data;
 
   data = String("wifiConn:close()");
   bool result = sendWaitForPrompt(data.c_str(), DISCONNECT_PROMPT);
-  
+
   sleep();
-  
+
   return result;
 }
 
-bool Sodaq_WifiBee::connect() {
+bool Sodaq_WifiBee::connect()
+{
   String data;
 
   data = String("wifi.setmode(wifi.STATION)");
@@ -249,7 +277,8 @@ bool Sodaq_WifiBee::connect() {
   return sendWaitForPrompt(data.c_str(), LUA_PROMPT);
 }
 
-bool Sodaq_WifiBee::disconnect() {
+bool Sodaq_WifiBee::disconnect()
+{
   String data;
 
   data = String("wifi.sta.disconnect()");
--- Sodaq_WifiBee.h
+++ Sodaq_WifiBee.h
@@ -1,24 +1,24 @@
 /* The MIT License (MIT)
 
-Copyright (c) <2015> <Gabriel Notman & M2M4ALL BV>
+ Copyright (c) <2015> <Gabriel Notman & M2M4ALL BV>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE. */
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE. */
 
 #ifndef SODAQ_WIFI_BEE_H_
 #define SODAQ_WIFI_BEE_H_
@@ -74,14 +74,15 @@ public:
 
   // HTTP methods
   // These use HTTP/1.1 and add headers for HOST (all) and Content-Length (except HTTPGet())
-  uint16_t HTTPAction(const char* server, const uint16_t port, const char* method, 
-    const char* location, const char* headers, const char* body);
+  uint16_t HTTPAction(const char* server, const uint16_t port,
+      const char* method, const char* location, const char* headers,
+      const char* body);
 
-  uint16_t HTTPGet(const char* server, const uint16_t port, const char* location,
-    const char* headers);
+  uint16_t HTTPGet(const char* server, const uint16_t port,
+      const char* location, const char* headers);
 
-  uint16_t HTTPPost(const char* server, const uint16_t port, const char* location,
-    const char* headers, const char* body);
+  uint16_t HTTPPost(const char* server, const uint16_t port,
+      const char* location, const char* headers, const char* body);
 
   // TCP methods
   bool openTCP(const char* server, uint16_t port);
@@ -108,11 +109,13 @@ private:
   void flushInputStream();
   void readForTime(const uint16_t timeMS);
   bool readTillPrompt(const char* prompt, const uint16_t timeMS);
-  
+
   void send(const char* data);
   bool sendWaitForPrompt(const char* data, const char* prompt);
 
-  bool openConnection(const char* server, const uint16_t port, const char* type);
+  bool openConnection(const char* server, const uint16_t port,
+      const char* type);
+  
   bool sendData(const uint8_t* data, const size_t length);
   bool closeConnection();
 
