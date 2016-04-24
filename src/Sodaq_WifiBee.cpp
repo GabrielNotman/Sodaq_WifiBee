5cce1a981b8a93eb227de4dfe911ecdb07ca690f
Gabriel Notman <g.notman@gmail.com>
2016-04-24 19:46:46 +0100

    Now the first packet of the server response is saved instead of the last.
    There are many issues with trying to concatenate the packets. The string type
    has a maximum length, and the concatenation process can result in running
    out of memory.

--- src/Sodaq_WifiBee.cpp
+++ src/Sodaq_WifiBee.cpp
@@ -48,9 +48,9 @@
 
 // Lua connection callback scripts
 #define OK_COMMAND "uart.write(0, \"OK\\r\\n\")"
-#define RECEIVED_CALLBACK "function(s, d) lastData=d print(d:len()..\"|DR|\") end" // Max length 231
+#define RECEIVED_CALLBACK "function(s, d) if lastData==nil then lastData=d end print(d:len()..\"|DR|\") end" // Max length 231
 #define STATUS_CALLBACK "print(\"|\" .. \"STS|\" .. wifi.sta.status() .. \"|\")" // Max length 255
-#define READ_BACK "uart.write(0, \"|\" .. \"SOF|\") for i=1, lastData:len(), 1 do uart.write(0, string.format(\"%02X\", lastData:byte(i))) tmr.wdclr() end uart.write(0, \"|EOF|\")" // Max length 255
+#define READ_BACK "uart.write(0, \"|\" .. \"SOF|\") for i=1, lastData:len(), 1 do uart.write(0, string.format(\"%02X\", lastData:byte(i))) tmr.wdclr() end lastData=nil uart.write(0, \"|EOF|\")" // Max length 255
 
 // Timeout constants
 #define RESPONSE_TIMEOUT 2000
